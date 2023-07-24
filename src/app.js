dccd69983b92e8c353bb420a50c542a1ebb70425
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-07-24 19:46:06 +0000

    Index, script and style files created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+// script.js
+const centerNumber = document.querySelector(".center-number")
+      ce
+// document.addEventListener("DOMContentLoaded", function() {
+//   const card = document.querySelector(".card");
+
+//   card.addEventListener("click", function() {
+//     card.classList.toggle("flipped");
+//   });
+// });
